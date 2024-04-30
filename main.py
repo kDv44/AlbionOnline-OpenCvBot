b146872a69bdf2c43b2aa07e18ecd002861d3064
ansi <morsbonaest@protonmail.com>
2024-04-30 15:53:24 +0300

    reserch

--- main.py
+++ main.py
@@ -2,70 +2,82 @@ import cv2 as cv
 import numpy as np
 import sys
 
-
 np.set_printoptions(threshold=sys.maxsize)
 
-stack_img = cv.imread("source_image\\albion_farm.jpg", cv.IMREAD_UNCHANGED)
-needle_img = cv.imread("source_image\\albion_cabbage.jpg", cv.IMREAD_UNCHANGED)
 
-result = cv.matchTemplate(stack_img, needle_img, cv.TM_SQDIFF_NORMED)
-print(result)
+def find_click_positions(
+    needle_img_path, haystack_img_path, threshold=0.5, debug_mode=None
+):
 
-threshold = 0.17
-locations = np.where(result <= threshold)
-locations = list(zip(*locations[::-1]))
-print(locations)
-
-if locations:
-    print("Found needle.")
+    haystack_img = cv.imread(haystack_img_path, cv.IMREAD_UNCHANGED)
+    needle_img = cv.imread(needle_img_path, cv.IMREAD_UNCHANGED)
 
     needle_w = needle_img.shape[1]
     needle_h = needle_img.shape[0]
 
-    line_color = (0, 255, 0)
-    line_type = cv.LINE_4
+    method = cv.TM_CCOEFF_NORMED
+    result = cv.matchTemplate(haystack_img, needle_img, method)
 
+    locations = np.where(result >= threshold)
+    locations = list(zip(*locations[::-1]))
+
+    rectangles = []
     for loc in locations:
-        top_left = loc
-        bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
-        cv.rectangle(stack_img, top_left, bottom_right, line_color, line_type)
+        rect = [int(loc[0]), int(loc[1]), needle_w, needle_h]
 
-    cv.imshow('Matches', stack_img)
-    cv.waitKey()
+        rectangles.append(rect)
+        rectangles.append(rect)
 
-else:
-    print("Needle not found.")
+    rectangles, weights = cv.groupRectangles(rectangles, groupThreshold=1, eps=0.5)
+
+    points = []
+    if len(rectangles):
+
+        line_color = (0, 255, 0)
+        line_type = cv.LINE_4
+        marker_color = (255, 0, 255)
+        marker_type = cv.MARKER_CROSS
+
+        for x, y, w, h in rectangles:
+
+            center_x = x + int(w / 2)
+            center_y = y + int(h / 2)
+
+            points.append((center_x, center_y))
+
+            if debug_mode == "rectangles":
+
+                top_left = (x, y)
+                bottom_right = (x + w, y + h)
+                cv.rectangle(
+                    haystack_img,
+                    top_left,
+                    bottom_right,
+                    color=line_color,
+                    lineType=line_type,
+                    thickness=2,
+                )
+            elif debug_mode == "points":
+                cv.drawMarker(
+                    haystack_img,
+                    (center_x, center_y),
+                    color=marker_color,
+                    markerType=marker_type,
+                    markerSize=40,
+                    thickness=2,
+                )
+
+        if debug_mode:
+            cv.imshow("Matches", haystack_img)
+            cv.waitKey()
+
+    return points
 
 
-'''
-# don't touch this (first_copper)
-
-# get the best match position
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-
-print("Best match top left position: %s" % str(max_loc))
-print("Best match confidence: %s" % max_val)
-
-threshold = 0.8
-if max_val >= threshold:
-    print("Found needle.")
-
-    # get dimesions the needle image
-    needle_w = needle_img.shape[1]
-    needle_h = needle_img.shape[0]
-
-    top_left = max_loc
-    bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
-
-    cv.rectangle(
-        stack_img, top_left, bottom_right,
-        color=(0, 255, 0), thickness=2, lineType=cv.LINE_4,
+if __name__ == "__main__":
+    search_points = find_click_positions(
+        "source_image\\albion_turnip.jpg",
+        "source_image\\albion_cabbage.jpg",
+        debug_mode="points",
     )
-    # this for looks
-    # cv.imshow('cabbage_result', stack_img)
-    # cv.waitKey()
-    cv.imwrite('source_image\\cabbage_result.jpg', stack_img)
-
-else:
-    print("Needle not found.")
-'''
+    print(search_points)
